fe42f93244215a2946fd3e16c6421e9f4e0ac36f
lhervieux <loris.hervieux@fioulmarket.fr>
2022-05-23 15:17:00 +0200

    Default values (props, propsType)

--- src/App/Components/Button/Button.js
+++ src/App/Components/Button/Button.js
@@ -1,16 +1,33 @@
 import React from "react";
 import style from './Buttons.module.css';
+import PropTypes from 'prop-types';
 
 function Button(props) {
     return <button className={style.Button}
     onClick={(evt) => {
-        props.onButtonClicked();
+        if (undefined !== props.onButtonClicked && 'function' === typeof props.onButtonClicked) {
+            props.onButtonClicked();
+        }
     }}
     style= {{
         backgroundColor:props.bgColor,
         color: props.color,
     }}
+    type={props.type}
     >{props.text}</button>;
+}
+
+Button.propTypes = {
+    bgColor: PropTypes.string.isRequired,
+    color: PropTypes.string.isRequired,
+    onButtonClicked: PropTypes.func,
+    type: PropTypes.string.isRequired,
+}
+
+Button.defaultProps = {
+    bgColor: 'tomato',
+    color: 'gold',
+    type: 'button',
 }
 
 export default Button;
